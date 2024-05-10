229419d79d5d691b736d558f9bdb269e712a70c0
renevelarde <renevelarde@me.com>
2024-05-10 16:16:56 -0700

    Update script.js
    added dark mode and split view

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 // script.js
 function loadFile() {
     const fileInput = document.getElementById('fileInput');
+    if (fileInput.files.length === 0) {
+        alert('Please select a file.');
+        return;
+    }
     const file = fileInput.files[0];
     const reader = new FileReader();
     reader.onload = function(e) {
         const textContent = document.getElementById('textContent');
-        textContent.innerHTML = e.target.result.replace(/\n/g, '<br>').replace(/(.+?)(<br>|$)/g, "<span>$1</span><br>");
+        textContent.innerText = e.target.result;
     };
     reader.readAsText(file);
-}
-
-function codeSelectedText() {
-    const selection = window.getSelection();
-    if (!selection.rangeCount) return;
-    let span = document.createElement("span");
-    span.className = "coded";
-    selection.getRangeAt(0).surroundContents(span);
-    let codedText = selection.toString();
-    document.getElementById('codedTextList').innerHTML += `<li>Coded Text: ${codedText}</li>`;
 }
